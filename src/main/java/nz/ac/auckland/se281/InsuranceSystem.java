08ecc2c9086c996d3b8d2260d99d308962df62dc
jaytipattni <jayti.pattni@gmail.com>
2023-03-14 22:56:45 +1300

    Created a database using arraylists and implemented logic to check username and age

--- src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -1,18 +1,45 @@
 package nz.ac.auckland.se281;
 
+import java.util.ArrayList;
+
+import org.eclipse.jgit.transport.CredentialItem.Username;
+
+// import java.util.ArrayList;
 import nz.ac.auckland.se281.Main.PolicyType;
 
 public class InsuranceSystem {
+  ArrayList<String> databaseUserName = new ArrayList<String>();
+  ArrayList<Integer> databaseAge = new ArrayList<Integer>();
+
   public InsuranceSystem() {
     // Only this constructor can be used (if you need to initialise fields).
   }
 
   public void printDatabase() {
-    // TODO: Complete this method.
+    // Don't forget case sensitive
+    // Print the number of people in database
+
   }
 
   public void createNewProfile(String userName, String age) {
-    // TODO: Complete this method.
+    // The username should be unique, and the age should be a positive number
+    // If these conditions are not met, an error message should be printed
+    if (databaseUserName.contains(userName) == false && Integer.valueOf(age) >= 0 && userName.length() > 3) {
+      databaseUserName.add(userName);
+      databaseAge.add(Integer.parseInt(age));
+    } else if (databaseUserName.contains(userName) == true) {
+      //Print not unique error
+      MessageCli.INVALID_USERNAME_TOO_SHORT.getMessage();
+    } else if (Integer.valueOf(age) < 0) {
+      //Print invalid age error message
+      MessageCli.INVALID_AGE.getMessage();
+    } else if (userName.length() <= 3) {
+      //Print userName too short error message
+      MessageCli.INVALID_USERNAME_TOO_SHORT.getMessage();
+    }
+    
+   System.out.println(databaseUserName);
+   System.out.println(databaseAge);
   }
 
   public void loadProfile(String userName) {
