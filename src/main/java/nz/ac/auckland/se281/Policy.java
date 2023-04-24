199ad7c722c8b767dabf9c58104de9dc52ba91fd
jaytipattni <jayti.pattni@gmail.com>
2023-04-24 20:40:39 +1200

    removed unnessary prints

--- src/main/java/nz/ac/auckland/se281/Policy.java
+++ src/main/java/nz/ac/auckland/se281/Policy.java
@@ -18,12 +18,12 @@ abstract class Policy {
     // If there are 2 policies in a profile, apply a 10% discount
     // If there are more than 2 policies in a profile, apply a 20% discount
     // Otherwise return the base premium without a discount
-    
+
     if (policyCount == 2) {
-      MessageCli.DISCOUNT_TWO.printMessage();
+      // MessageCli.DISCOUNT_TWO.printMessage();
       return (90 * basePremium()) / 100;
     } else if (policyCount >= 3) {
-      MessageCli.DISCOUNT_MULTIPLE.printMessage();
+      // MessageCli.DISCOUNT_MULTIPLE.printMessage();
       return (80 * basePremium()) / 100;
     } else {
       return basePremium();
